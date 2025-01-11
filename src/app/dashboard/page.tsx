ea1f7243ddfe1fc2aaeb3fc589d332f2b31e4b98
thani35 <j.thanigaive@stetig.in>
2025-01-11 16:26:12 +0530

    Dashboard page done

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AuthUserReusableCode from "@/components/AuthUserReusableCode";
+import CustomizedInputWithLabel from "@/components/CustomizedInputWithLabel";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { dashboardSchema } from "@/lib/zod";
@@ -12,39 +13,24 @@ type FormData = z.infer<typeof dashboardSchema>;
 
 const dashboard = () => {
 
-  const { register, handleSubmit, formState: { errors }, getValues } = useForm<FormData>({
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: zodResolver(dashboardSchema),
   });
 
+  const onSubmit = (data: FormData) => {
+    console.log('i m called')
+  }
+
   return (
-    // <AuthUserReusableCode pageTitle='Dashboard'>
-    <form className="flex items-center space-x-4">
-      <div className="w-1/4">
-        <label htmlFor="start-date" className="block text-sm font-medium text-gray-700">
-          From Date
-        </label>
-        <Input
-          {...register('start')}
-          type="date"
-          id="start-date"
-          className="mt-1 block w-full"
-        />
-      </div>
-      <div className="w-1/4">
-        <label htmlFor="end-date" className="block text-sm font-medium text-gray-700">
-          To Date
-        </label>
-        <Input
-          type="date"
-          id="end-date"
-          className="mt-1 block w-full"
-        />
-      </div>
-      <Button variant="default" className="px-4 py-2 self-end">
-        Search
-      </Button>
-    </form>
-    // </AuthUserReusableCode >
+    <AuthUserReusableCode pageTitle='Dashboard'>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex items-center space-x-4">
+        <CustomizedInputWithLabel containerClass={'w-1/4'} {...register('fromDate')} errors={errors.fromDate} label='From Date' type='date' />
+        <CustomizedInputWithLabel containerClass={'w-1/4'} {...register('toDate')} errors={errors.toDate} label='To Date' type='date' />
+        <Button variant="default" className="px-4 py-2 self-end">
+          Search
+        </Button>
+      </form>
+    </AuthUserReusableCode >
   );
 }
 
