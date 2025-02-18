feb1f7eb2c8973511a4507b8432cad3bd6549057
thani35 <j.thanigaive@stetig.in>
2025-02-18 16:43:24 +0530

    Added dynamic breadscrum

--- src/components/CustomBreadcrumb.tsx
+++ src/components/CustomBreadcrumb.tsx
@@ -5,16 +5,43 @@ import { SidebarTrigger } from './ui/sidebar'
 import { LogOut, Wallet } from 'lucide-react';
 import { Button } from './ui/button';
 import { Card } from './ui/card';
+import { usePathname } from 'next/navigation';
 
 interface CustomBreadcrumbProps {
     pageTitle: string;
     children: React.ReactNode,
     onSignOut: (event: React.MouseEvent) => Promise<void>;
     agencyBalanceDetail: any;
+    blacklist?: string[];
 }
 
-const CustomBreadcrumb: React.FC<CustomBreadcrumbProps> = ({ pageTitle, children, onSignOut, agencyBalanceDetail }) => {
+const CustomBreadcrumb: React.FC<CustomBreadcrumbProps> = ({
+    pageTitle, children, onSignOut, agencyBalanceDetail,
+    blacklist = ["agency", 'dashboard', 'admin', 'department']
+}) => {
+    const router = usePathname();
+    const pathSegments = router.split("/").filter((seg) => seg !== "");
 
+    // const filteredSegments = pathSegments.filter((segment) => !blacklist.includes(segment));
+
+    const breadcrumbLinks = pathSegments.map((segment, index) => {
+        const href = "/" + pathSegments.slice(0, index + 1).join("/");
+        const isLast = index === pathSegments.length - 1;
+
+        return blacklist.includes(segment) ? null : (
+            <React.Fragment key={href}>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    {isLast ? (
+                        <BreadcrumbPage className='capitalize'>{segment.replaceAll('-', ' ')}</BreadcrumbPage>
+                    ) : (
+                        <BreadcrumbLink className='capitalize' href={href}>{segment.replaceAll('-', ' ')}</BreadcrumbLink>
+                    )}
+                </BreadcrumbItem>
+
+            </React.Fragment>
+        );
+    });
     return (
         <div className='flex flex-col h-screen bg-lightThemeColor p-2'>
             <Card className="flex sticky top-0 bg-background h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -59,12 +86,9 @@ const CustomBreadcrumb: React.FC<CustomBreadcrumbProps> = ({ pageTitle, children
                 <Breadcrumb className='ml-auto'>
                     <BreadcrumbList>
                         <BreadcrumbItem className="hidden md:block">
-                            <BreadcrumbLink href="#">Home</BreadcrumbLink>
+                            <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
                         </BreadcrumbItem>
-                        <BreadcrumbSeparator className="hidden md:block" />
-                        <BreadcrumbItem>
-                            <BreadcrumbPage>Dashboard</BreadcrumbPage>
-                        </BreadcrumbItem>
+                        {breadcrumbLinks}
                     </BreadcrumbList>
                 </Breadcrumb>
             </Card >
