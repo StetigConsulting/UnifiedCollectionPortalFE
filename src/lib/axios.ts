22c25a2e75a9a0600e57da1e83e55f4b4f610f7d
thani35 <j.thanigaive@stetig.in>
2025-02-28 10:29:11 +0530

    Added interceptor for authentication module

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_V2,
@@ -6,5 +7,18 @@ const api = axios.create({
         'Content-Type': 'application/json',
     },
 });
+
+api.interceptors.request.use(
+    async (config) => {
+        const session = await getSession();
+        if (session && session.user.accessToken) {
+            config.headers["Authorization"] = `Bearer ${session.user.accessToken}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default api;
