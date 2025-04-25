f6e7031fd5b1d69937401ac4896420631a06c664
thani35 <j.thanigaive@stetig.in>
2025-04-25 20:26:10 +0530

    Temporary fixes

--- src/app/debug-env/page.tsx
+++ src/app/debug-env/page.tsx
@@ -1,10 +1,15 @@
 // app/debug-env/page.tsx
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+const { backendUrlConfig } = publicRuntimeConfig;
+
 export default function DebugPage() {
     return (
         <div>
             <p><strong>API Base URL:</strong> {process.env.NEXT_PUBLIC_API_BASE_URL}</p>
             <p><strong>API Base URL V2:</strong> {process.env.NEXT_PUBLIC_API_BASE_URL_V2}</p>
             <p><strong>Secret:</strong> {process.env.AUTH_SECRET}</p>
+            <p>backend : {backendUrlConfig}</p>
         </div>
     );
 }
