8130cd07100c6400d6c633fce86c130524ad4ceb
thani35 <j.thanigaive@stetig.in>
2025-04-29 19:16:28 +0530

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // output: 'export',  // Tells Next.js to prepare a static export
-  // Add any other custom configuration here if needed
 };
 
 export default nextConfig;
