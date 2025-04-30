2956bbe606f237990f18426b1726b99507805c5e
thani35 <j.thanigaive@stetig.in>
2025-04-30 13:03:23 +0530

    Fixed

--- src/app/debug-env/page.tsx
+++ src/app/debug-env/page.tsx
@@ -3,8 +3,8 @@ export default function DebugPage() {
         <div>
             <p><strong>API Base URL NEXT JS Frontend:</strong> {process.env.NEXT_PUBLIC_API_BASE_URL_FRONTEND}</p>
             <p><strong>API Base URL NEXT JS Backend:</strong> {process.env.NEXT_PUBLIC_API_BASE_URL_BACKEND}</p>
-            <p><strong>Backend APP:</strong> {process.env.NEXT_PUBLIC_FRONTEND_APP}</p>
-            <p><strong>Frontend APP:</strong> {process.env.NEXT_PUBLIC_BACKEND_APP}</p>
+            <p><strong>Frontend APP:</strong> {process.env.NEXT_PUBLIC_FRONTEND_APP}</p>
+            <p><strong>Backend APP:</strong> {process.env.NEXT_PUBLIC_BACKEND_APP}</p>
             <p><strong>Secret:</strong> {process.env.AUTH_SECRET}</p>
         </div>
     );
